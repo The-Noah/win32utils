2e6725af54f3086c30ba3cd82bd3bfa44cee9914
The Noah <thenoahbz@gmail.com>
2024-07-25 11:38:18 -0500

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# win32utils
+
+A collection of opinionated utilities for working with the Windows API. Still in early development.
---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use windows::core::PCWSTR;
+use windows::Win32::UI::WindowsAndMessaging::{
+  MessageBoxW, IDNO, IDOK, IDYES, MB_ICONERROR, MB_ICONINFORMATION, MB_ICONQUESTION, MB_ICONWARNING, MB_OK, MB_YESNO, MESSAGEBOX_RESULT, MESSAGEBOX_STYLE,
+};
+
+pub enum DialogIcon {
+  Info = MB_ICONINFORMATION.0 as isize,
+  Question = MB_ICONQUESTION.0 as isize,
+  Warning = MB_ICONWARNING.0 as isize,
+  Error = MB_ICONERROR.0 as isize,
+}
+
+pub enum DialogButtons {
+  Ok = MB_OK.0 as isize,
+  YesNo = MB_YESNO.0 as isize,
+}
+
+pub enum DialogResult {
+  Ok = IDOK.0 as isize,
+  Yes = IDYES.0 as isize,
+  No = IDNO.0 as isize,
+}
+
+impl From<MESSAGEBOX_RESULT> for DialogResult {
+  fn from(value: MESSAGEBOX_RESULT) -> Self {
+    match value {
+      IDOK => DialogResult::Ok,
+      IDYES => DialogResult::Yes,
+      IDNO => DialogResult::No,
+      _ => panic!("Invalid dialog result"),
+    }
+  }
+}
+
+/// Display a message box dialog.
+pub fn dialog(title: impl ToString, text: impl ToString, icon: DialogIcon, buttons: DialogButtons) -> DialogResult {
+  let title: Vec<u16> = (title.to_string() + "\0").encode_utf16().collect();
+  let text: Vec<u16> = (text.to_string() + "\0").encode_utf16().collect();
+
+  unsafe {
+    MessageBoxW(
+      None,
+      PCWSTR(text.as_ptr()),
+      PCWSTR(title.as_ptr()),
+      MESSAGEBOX_STYLE(icon as u32) | MESSAGEBOX_STYLE(buttons as u32),
+    )
+    .into()
+  }
+}
